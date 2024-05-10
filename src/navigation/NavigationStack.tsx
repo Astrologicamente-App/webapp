ff105356259a3c738b55b0dffc72968dade61958
JoaoVictorSeverinodosSantos <joaovictorseverinodossantos@gmail.com>
2024-05-09 23:43:06 -0300

    login screen done needs navigation

--- src/navigation/NavigationStack.tsx
+++ src/navigation/NavigationStack.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import HomeScreen from '../screens/HomeScreen';
+import LoginScreen from '../screens/LoginScreen';
+import RegisterScreen from '../screens/RegisterScreen';
 
 const Stack = createStackNavigator();
 
 const NavigationStack = () => {
   return (
     <Stack.Navigator>
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Register" component={RegisterScreen} />
       <Stack.Screen name="Home" component={HomeScreen} />
-      {/* Outras telas aqui */}
     </Stack.Navigator>
   );
 }
